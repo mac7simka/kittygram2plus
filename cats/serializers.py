15d2adcb0f06c5ff0122c7945fe5cd02ff65e0fc
Maksim Karaskin <mac7sim@gmail.com>
2024-06-12 22:22:37 +0300

    кот

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,7 +1,7 @@
+import datetime as dt
+
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
-
-import datetime as dt
 
 from .models import CHOICES, Achievement, AchievementCat, Cat, User
 
@@ -27,7 +27,7 @@ class CatSerializer(serializers.ModelSerializer):
     achievements = AchievementSerializer(many=True, required=False)
     color = serializers.ChoiceField(choices=CHOICES)
     age = serializers.SerializerMethodField()
-    
+
     class Meta:
         model = Cat
         fields = ('id', 'name', 'color', 'birth_year', 'achievements', 'owner',
